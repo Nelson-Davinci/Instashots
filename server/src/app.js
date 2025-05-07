1d677960ba9ee3b8cf174fa47d734aecb42bf964
Nelson David <nwanonezedavid@gmail.com>
2025-05-07 21:22:20 +0100

    Trying to figure out the deployment

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,11 @@ import commentRoutes from "./routes/comment.js";
 
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:4600", "https://instashots.vercel.app"],
+  origin: [
+    "http://localhost:4600",
+    "https://instashots.vercel.app",
+    "https://instashots-01.vercel.app",
+  ],
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
