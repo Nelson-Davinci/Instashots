45d31f82bd9ebed8f26711534988d43461540c1a
Nelson David <nwanonezedavid@gmail.com>
2025-05-07 22:21:54 +0100

    The deliverable we got

--- server/src/app.js
+++ server/src/app.js
@@ -10,11 +10,7 @@ import commentRoutes from "./routes/comment.js";
 
 const app = express();
 const corsOptions = {
-  origin: [
-    "http://localhost:4600",
-    "https://instashots.vercel.app",
-    "https://instashots-01.vercel.app",
-  ],
+  origin: ["http://localhost:4600", "https://instashots.vercel.app"],
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
