3be327c1f4af1190ab91302f5a68dd18dd92a5dc
Nelson David <nwanonezedavid@gmail.com>
2025-05-07 17:04:10 +0100

    The first deliverable project

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ import commentRoutes from "./routes/comment.js";
 
 const app = express();
 const corsOptions = {
-  origin: ["http://localhost:4600", "https://instashots.vercel.app/"],
+  origin: ["http://localhost:4600", "https://instashots.vercel.app"],
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
